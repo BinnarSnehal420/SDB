98d37f166a465af67ad286e7670f8e0619596a46
BinnarSnehal420 <catkityognature17@gmail.com>
2026-04-01 21:54:42 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/payment", require("./routes/paymentRoutes"));
+app.use("/api/recommend", require("./routes/recommendRoutes"));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
